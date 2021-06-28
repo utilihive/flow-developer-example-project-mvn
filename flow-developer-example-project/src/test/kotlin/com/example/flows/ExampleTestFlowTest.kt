ca4608b77194521368ea21d1b809f2a9c2ded05d
Concourse CI <concourse@greenbird.com>
2021-06-28 17:48:29 +0000

    Version 0.14.0

--- flow-developer-example-project/src/test/kotlin/com/example/flows/ExampleTestFlowTest.kt
+++ flow-developer-example-project/src/test/kotlin/com/example/flows/ExampleTestFlowTest.kt
@@ -3,9 +3,7 @@ package com.example.flows
 import com.example.flows.ExampleTestFlow.exampleOpenApiDoc
 import com.example.flows.ExampleTestFlow.exampleResourceKey
 import com.example.flows.ExampleTestFlow.exampleSpec
-import com.greenbird.utilihive.integration.flowdeveloper.sdk.flow.FlowEditor.forTestContext
 import com.greenbird.utilihive.integration.flowdeveloper.sdk.resources.Resource
-import com.greenbird.utilihive.integration.flowdeveloper.sdk.server.UtilihiveRestClientFactory
 import com.greenbird.utilihive.integration.flowdeveloper.sdk.server.UtilihiveRestClientFactory.basicAuth
 import com.greenbird.utilihive.integration.flowdeveloper.sdk.testing.FlowTestManager
 import com.greenbird.utilihive.integration.flowdeveloper.sdk.testing.addFlowTestConfig
@@ -15,7 +13,7 @@ import com.greenbird.utilihive.integration.test.concurrent.core.ConcurrentTestCo
 import com.greenbird.utilihive.integration.test.concurrent.core.junit5.ConcurrentTestBase
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import javax.ws.rs.client.Entity
+import javax.ws.rs.client.Entity.json
 
 class ExampleTestFlowTest : ConcurrentTestBase() {
 
@@ -24,13 +22,12 @@ class ExampleTestFlowTest : ConcurrentTestBase() {
         ctx: ConcurrentTestContext
     ) {
         val resource = Resource(key = exampleResourceKey, content = exampleOpenApiDoc)
-        val flowSpec = exampleSpec.forTestContext(ctx)
-        ctx.addFlowTestConfig { resource(resource); flow(flowSpec) }
+        ctx.addFlowTestConfig { resource(resource); flow(exampleSpec) }
 
         FlowTestManager.flowTest(ctx) {
             val value = "testValue"
-            val responseMessage = UtilihiveRestClientFactory.flowRestEndpoint(flowSpec).path("echo").request()
-                .basicAuth().post(Entity.json(SimpleValue(value)), SimpleMessage::class.java)
+            val responseMessage = restApiEndpoint(exampleSpec).path("echo").request()
+                .basicAuth().post(json(SimpleValue(value)), SimpleMessage::class.java)
             assertThat(responseMessage.message).isEqualTo(value)
         }
     }
